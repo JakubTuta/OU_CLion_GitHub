274b7a1bd6130d4a577ac0d40453105bf64920aa
JakubTuta <jakubtuta@o2.pl>
2022-01-17 17:55:10 +0100

    Zmiana na mnożenie w metodzie policz

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+int policz(int a, int b)
+{
+    return a*b;
+}
+
 int main()
 {
     printf("Hello, World!\n");
+    printf("W branchu 1: 2 * 3 = %d\n", policz(2,3));
     return 0;
 }
